e127477a48023ac167d2f634c17b708d12ef75e1
Maor <maorazoulay@gmail.com>
2022-04-07 16:28:02 -0400

    test for advanced collectible

---
+++ tests/unit/test_advanced_collectible.py
@@ -0,0 +1,34 @@
+from brownie import network, AdvancedCollectible
+import pytest
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+    get_breed,
+)
+from scripts.advanced_collectible.deploy_and_create import deploy_and_create
+
+
+def test_can_create_advanced_collectible():
+    # deploy the contract
+    # create an NFT
+    # get random breed back
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for integration testing")
+    # Acting step
+    random_number = 777
+    advanced_collectible, creation_transaction = deploy_and_create()
+    requestId = creation_transaction.events["requestedCollectible"]["requestId"]
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        requestId, random_number, advanced_collectible.address, {"from": get_account()}
+    )
+    # Assert
+    assert advanced_collectible.tokenCounter() == 1
+    assert advanced_collectible.tokenIdToBreed(0) == random_number % 3
+
+
+def test_get_breed():
+    # Arrange / Act
+    breed = get_breed(0)
+    # Assert
+    assert breed == "PUG"
